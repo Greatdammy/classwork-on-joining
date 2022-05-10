b3aa3ca930ceb0999b24971b23d2afcd271b21f7
DAMILARE OBAFEMI <damimmanuel7@gmail.com>
2022-05-10 13:48:59 +0100

    Add project files.

---
+++ classwork-joining/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace classwork_joining
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var allSales = Sales.GetSales();
+            var allItems = Items.GetItems();
+
+
+            var joinItems = allSales.GroupJoin(allItems, x => x.Id,
+                                                          y => y.SalesId,
+                                                          (x, groupedItems) => new
+                                                          {
+                                                              x.Id,
+                                                           x.CategoryName,
+                                                           groupedItems
+   
+                                                          });
+            foreach (var item in joinItems)
+            {
+                Console.WriteLine($"CategoryName: {item.CategoryName}");
+                foreach (var item2 in item.groupedItems)
+                {
+                    Console.WriteLine($"Item Name: {item2.ItemName}, Id: {item2.SalesId}, Roll no: {item2.RollNumber}");
+                }
+                Console.WriteLine();
+            }
+
+            var sum = allItems.Sum(x => x.Amount);
+            Console.WriteLine($"Total Amount: {sum}");
+            Console.WriteLine();
+
+            var averageNo = allItems.Average(x => x.RollNumber);
+            Console.WriteLine($"Average Number: {averageNo}");
+
+        }
+    }
+}
